90b91203a2ad1aed8693d280c0f230344d9e1706
Krzysztof Magosa <krzysztof@magosa.pl>
2014-12-16 00:38:14 +0100

    new: Pin class

---
+++ include/miniberry/Pin.hpp
@@ -0,0 +1,54 @@
+#ifndef MINIBERRY_PIN_HPP
+#define MINIBERRY_PIN_HPP
+
+#include "miniberry/Port.hpp"
+#include <avr/io.h>
+#include <stdint.h>
+
+namespace miniberry {
+
+    class Pin
+    {
+    public:
+        Pin(Port& port, uint8_t no): port(port), no(no)
+        {
+        }
+        
+        void setValue(bool const value)
+        {
+            if (value) {
+                *port.port |= (1 << no);
+            }
+            else {
+                *port.port &= ~(1 << no);
+            }
+        }
+
+        bool getValue()
+        {
+            return *port.pin & (1 << no);
+        }
+
+        void setPullUp(bool const value)
+        {
+            setValue(value);
+        }
+
+        void makeInput()
+        {
+            *port.ddr &= ~(1 << no);
+        }
+
+        void makeOutput()
+        {
+            *port.ddr |= (1 << no);
+        }
+        
+    protected:
+        Port port;
+        int const no;
+    };
+
+}
+
+#endif
